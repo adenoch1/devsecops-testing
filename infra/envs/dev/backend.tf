b7367452627830cbdcbe4a519b43dff1697f8e56
adenoch1 <enochadekanye@gmail.com>
2026-02-01 22:26:30 -0500

    Adding the right bucket name

--- infra/envs/dev/backend.tf
+++ infra/envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "devsecops-project1-tfstate-enoch-2026"
+    bucket         = "devsecops-testing-tfstate-enoch-2026"
     key            = "dev/envs/dev/terraform.tfstate"
     region         = "ca-central-1"
     dynamodb_table = "devsecops-project1-tflocks"
