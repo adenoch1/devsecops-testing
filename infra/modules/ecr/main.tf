545c5032fe5efea8c62e860f47aad200a12aad5c
adenoch1 <enochadekanye@gmail.com>
2026-02-02 06:38:15 -0500

    chore: terraform fmt

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -1,7 +1,7 @@
 resource "aws_ecr_repository" "this" {
   name                 = "${var.name_prefix}-repo"
   image_tag_mutability = "IMMUTABLE"
-  force_delete = true
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
