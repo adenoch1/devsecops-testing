fb036277278410766930945e4d6de32c70ab0ce2
adenoch1 <enochadekanye@gmail.com>
2026-02-04 11:34:22 -0500

    Updated done

--- infra/modules/logging/outputs.tf
+++ infra/modules/logging/outputs.tf
@@ -1,29 +1,19 @@
-output "alb_logs_bucket_name" {
-  description = "S3 bucket name where ALB access logs are stored"
-  value       = aws_s3_bucket.alb_logs.bucket
+output "alb_log_prefix" {
+  value       = var.alb_log_prefix
+  description = "Prefix inside the S3 bucket for ALB access logs"
 }
 
-output "alb_logs_access_bucket_name" {
-  description = "S3 bucket name used for S3 access logging"
-  value       = aws_s3_bucket.alb_logs_access.bucket
+output "alb_logs_bucket_name" {
+  description = "Name of the S3 bucket that receives ALB access logs"
+  value       = aws_s3_bucket.alb_logs.bucket
 }
 
 output "cloudwatch_logs_kms_key_arn" {
   description = "KMS key ARN used to encrypt CloudWatch log groups"
-  value       = aws_kms_key.alb_logs.arn
+  value       = aws_kms_key.cloudwatch_logs.arn
 }
 
 output "alb_logs_kms_key_arn" {
-  description = "KMS key ARN used to encrypt ALB logs buckets"
+  description = "KMS key ARN used to encrypt ALB log buckets"
   value       = aws_kms_key.alb_logs.arn
-}
-
-output "vpc_flow_log_group_name" {
-  description = "CloudWatch log group name for VPC flow logs"
-  value       = aws_cloudwatch_log_group.vpc_flow.name
-}
-
-output "alb_logs_events_queue_arn" {
-  description = "SQS queue ARN used for ALB log event notifications"
-  value       = aws_sqs_queue.alb_logs_events.arn
 }
