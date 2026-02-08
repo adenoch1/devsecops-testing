b672a7143f59eb46e14f4a821ba30269e68de0eb
adenoch1 <enochadekanye@gmail.com>
2026-02-08 11:16:37 -0500

    Updates

--- infra/modules/logging/outputs.tf
+++ infra/modules/logging/outputs.tf
@@ -8,14 +8,19 @@ output "alb_log_access_bucket_name" {
   value       = aws_s3_bucket.alb_logs_access.bucket
 }
 
+output "alb_log_audit_bucket_name" {
+  description = "S3 bucket name that receives access logs from the access bucket (audit bucket)"
+  value       = aws_s3_bucket.alb_logs_audit.bucket
+}
+
+output "alb_log_audit_access_bucket_name" {
+  description = "Terminal S3 bucket name that receives access logs for the audit bucket (end of chain)"
+  value       = aws_s3_bucket.alb_logs_audit_access.bucket
+}
+
 output "alb_logs_kms_key_arn" {
   description = "KMS CMK ARN used for encrypting the ALB log buckets (SSE-KMS)"
   value       = aws_kms_key.alb_logs.arn
-}
-
-output "s3_log_events_topic_arn" {
-  description = "SNS topic ARN that receives S3 log bucket event notifications"
-  value       = aws_sns_topic.s3_log_events.arn
 }
 
 output "cloudwatch_logs_kms_key_arn" {
