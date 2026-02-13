378aed1815f031eea7588eb1e44ae26d22249510
adenoch1 <enochadekanye@gmail.com>
2026-02-12 19:42:34 -0500

    Update now

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -689,7 +689,6 @@ resource "aws_kinesis_firehose_delivery_stream" "waf" {
 }
 
 resource "aws_wafv2_web_acl_logging_configuration" "alb" {
-  provider     = aws
   resource_arn = aws_wafv2_web_acl.alb.arn
 
   log_destination_configs = [
@@ -701,6 +700,7 @@ resource "aws_wafv2_web_acl_logging_configuration" "alb" {
     aws_kinesis_firehose_delivery_stream.waf
   ]
 }
+
 
 
 
