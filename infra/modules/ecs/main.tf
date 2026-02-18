662b9b91b1dabfd98a35f70a97c0238316ad46fc
adenoch1 <enochadekanye@gmail.com>
2026-02-18 11:18:31 -0500

    Just checking

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -10,7 +10,7 @@ locals {
 
 # ----------------------------------
 # CloudWatch Logs (encrypted with KMS)
-# --------------------------------
+# ----------------------------------
 resource "aws_cloudwatch_log_group" "app" {
   name              = "/ecs/${var.name_prefix}"
   retention_in_days = var.log_retention_days
